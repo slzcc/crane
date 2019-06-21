73703abc8196555c4e571fa4e8321ab7b5edc2fe
slzcc <383821949@qq.com>
2019-06-21 15:27:59 +0800

    update PushKubernetesNodeBinaries.sh

--- script/PushKubernetesNodeBinaries.sh
+++ script/PushKubernetesNodeBinaries.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-export k8sVersion=${k8sVersion:-'v1.14.2'}
-export cniVersion=${cniVersion:-'v0.7.5'}
+_k8sVersion=`awk '/k8s_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_cni_version=`awk '/cni_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_etcdVersion=`awk '/etcd_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_pauseVersion=`awk '/pause_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_calicoVersion=`awk '/calico_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_haproxyVersion=`awk '/haproxy_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
+_corednsVersion=`awk '/dns_version/{print}' ../group_vars/all.yml | awk -F': ' '{print $2}' | sed -r "s/'//g"`
 
-export etcdVersion=${etcdVersion:-'3.3.10'}
-export pauseVersion=${pauseVersion:-'3.1'}
-export calicoVersion=${calicoVersion:-'v3.7.2'}
-export haproxyVersion=${haproxyVersion:-'2.0.0'}
-export corednsVersion=${corednsVersion:-'1.5.0'}
+export dockercliVersion=18.09
+
+export k8sVersion=${_k8sVersion:-'v1.14.2'}
+export cniVersion=${_cni_version:-'v0.7.5'}
+
+export etcdVersion=${_etcdVersion:-'3.3.10'}
+export pauseVersion=${_pauseVersion:-'3.1'}
+export calicoVersion=${_calicoVersion:-'v3.7.2'}
+export haproxyVersion=${_haproxyVersion:-'2.0.0'}
+export corednsVersion=${_corednsVersion:-'1.5.0'}
 
 export targetRegistry=${targetRegistry:-'slzcc'}
 
@@ -27,7 +37,7 @@ EOF
 chmod +x ${temporaryDirs}/docker-image-import.sh
 
 cat > ${temporaryDirs}/Dockerfile << EOF
-FROM docker:18.09 as DockerCli
+FROM docker:${dockercliVersion} as DockerCli
 
 FROM ubuntu:16.04
 
