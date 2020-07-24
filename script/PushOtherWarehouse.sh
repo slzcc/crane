983fa285b344d04a11ab5f8ab0df7991ce4fd66d
shileizcc.com <383821949@qq.com>
2020-07-24 10:03:04 +0800

    update github workflows

--- script/PushOtherWarehouse.sh
+++ script/PushOtherWarehouse.sh
@@ -6,22 +6,22 @@ BUILD_VERSION=${BUILD_VERSION:-''}
 
 if [ "${PUSH_OTHER_REGISTRY_CHECK_PERFORM}" == "true" ]; then
 
-    docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
+    sudo docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
                docker.pkg.github.com/${targetRegistry}/crane/kubernetes:${BUILD_VERSION}
-    docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
+    sudo docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
                registry.cn-beijing.aliyuncs.com/slzcc/kubernetes:${BUILD_VERSION}
-    docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
+    sudo docker tag ${targetRegistry}/kubernetes:${BUILD_VERSION} \
                registry.cn-hangzhou.aliyuncs.com/slzcc/kubernetes:${BUILD_VERSION}
 
-    docker push docker.pkg.github.com/${targetRegistry}/crane/kubernetes:${BUILD_VERSION}
+    sudo docker push docker.pkg.github.com/${targetRegistry}/crane/kubernetes:${BUILD_VERSION}
 
-    docker push registry.cn-beijing.aliyuncs.com/{targetRegistry}/kubernetes:${BUILD_VERSION}
+    sudo docker push registry.cn-beijing.aliyuncs.com/{targetRegistry}/kubernetes:${BUILD_VERSION}
 
-    docker push registry.cn-hangzhou.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
+    sudo docker push registry.cn-hangzhou.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
 
-    docker rmi -f docker.pkg.github.com/${targetRegistry}/crane/kubernetes:${BUILD_VERSION}
+    sudo docker rmi -f docker.pkg.github.com/${targetRegistry}/crane/kubernetes:${BUILD_VERSION}
 
-    docker rmi -f registry.cn-beijing.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
+    sudo docker rmi -f registry.cn-beijing.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
 
-    docker rmi -f registry.cn-hangzhou.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
+    sudo docker rmi -f registry.cn-hangzhou.aliyuncs.com/${targetRegistry}/kubernetes:${BUILD_VERSION}
 fi
